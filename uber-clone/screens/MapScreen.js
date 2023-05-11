6fcdb31178feca62c111445f442c3cc20b55bedd
yugaldekate72 <yugal.dekate@ajatus.co.in>
2023-05-11 12:47:38 +0530

    map-view navigation-card ride-options-card

--- uber-clone/screens/MapScreen.js
+++ uber-clone/screens/MapScreen.js
@@ -2,16 +2,39 @@ import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import tw from 'tailwind-react-native-classnames';
+import Map from '../components/Map';
+import NavigateCard from '../components/NavigateCard';
+import RideOptionsCard from '../components/RideOptionsCard';
 
 const MapScreen = () => {
     const Stack = createStackNavigator();
 
   return (
-    <SafeAreaView style={[tw`bg-white h-full`]} >
-        <View style={tw`p-5`} >
-            <Text>Hii Mpas</Text>
+    <View style={[tw`bg-white h-full`]} >
+    
+        <View style={tw`h-1/2`} >
+          <Map/>
         </View>
-    </SafeAreaView>
+
+        <View style={tw`h-1/2`} >
+          <Stack.Navigator>
+            <Stack.Screen
+              name="NavigationCard"
+              component={NavigateCard}
+              options={{
+                hearderShown:false
+              }}
+            />
+            <Stack.Screen
+              name="RideOptionsCard"
+              component={RideOptionsCard}
+              options={{
+                hearderShown:false
+              }}
+            />
+          </Stack.Navigator>
+        </View>
+    </View>
   )
 }
 
