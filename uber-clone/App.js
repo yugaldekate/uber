f9c91a8e3842f998f2717560b5114b02a9d65fbf
yugaldekate72 <yugal.dekate@ajatus.co.in>
2023-05-13 17:17:56 +0530

    added keyboard-avoiding-view

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -1,5 +1,6 @@
 import { Provider } from 'react-redux';
 import {store} from './store';
+import { KeyboardAvoidingView } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import MapScreen from './screens/MapScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -16,22 +17,25 @@ export default function App() {
     <Provider store={store} >
       <NavigationContainer>
         <SafeAreaProvider>
-          <Stack.Navigator>
-            <Stack.Screen
-              name='HomeScreen'
-              component={HomeScreen}
-              options={{
-                headerShown:false
-              }}
-            />
-            <Stack.Screen
-              name='MapScreen'
-              component={MapScreen}
-              options={{
-                headerShown:false
-              }}
-            />
-          </Stack.Navigator>
+          <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{flex:1}} 
+          keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0} >
+            <Stack.Navigator>
+              <Stack.Screen
+                name='HomeScreen'
+                component={HomeScreen}
+                options={{
+                  headerShown:false
+                }}
+              />
+              <Stack.Screen
+                name='MapScreen'
+                component={MapScreen}
+                options={{
+                  headerShown:false
+                }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
